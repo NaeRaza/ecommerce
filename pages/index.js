7a2a32a3074a8284c1d15b023a844dd5a3762a5b
NaeRaza <naerazafimbelo@gmail.com>
2023-08-17 17:41:05 +0300

    Color button

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,17 @@ export default function Home() {
     return (
       <div className="bg-blue-900 w-screen h-screen flex items-center">
         <div className="text-center w-full">
-          <button onClick={()=> signIn('google')} className="bg-white p-2 rounded-lg">Login with Google</button>
+          <button onClick={()=> signIn('google')} className="bg-white p-2 rounded-lg hover:bg-red-500 hover:text-white duration-200 hover:scale-110">Login with Google</button>
         </div>
       </div>
     )
   }
 
   return(
-    <div>logged in {session.user.email}</div>
+    <div className="bg-white-900 w-screen h-screen">
+      <div className="">logged in {session.user.email}</div>
+      <button onClick={() => signOut()} className="bg-red-900 text-white p-2 rounded-lg hover:bg-violet-600 duration-200 hover:scale-110">Sign out</button>
+    </div>
   )
  
 }
