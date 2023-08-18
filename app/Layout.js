f9c2313c47375f4d114b199cfdf4fad8ea665df5
NaeRaza <naerazafimbelo@gmail.com>
2023-08-18 18:34:01 +0300

    Ajout sidebar

---
+++ app/Layout.js
@@ -0,0 +1,27 @@
+import Nav from "@/components/Nav";
+import { useSession, signIn } from "next-auth/react"
+import { FcGoogle } from 'react-icons/fc'
+
+export default function Layout({children}) {
+
+  const { data: session } = useSession();
+
+  if(!session){
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full flex justify-center">
+          <button onClick={()=> signIn('google')} className="bg-white p-2 rounded-lg hover:bg-gray-200 hover:text-black duration-200 hover:scale-110 w-50 flex justify-center items-center"><FcGoogle className="mr-2"/> <span>Login with Google</span> 
+          </button>
+        </div>
+      </div>
+    )
+  }
+
+  return(
+    <div className="bg-blue-900 min-h-screen flex">
+      <Nav />
+      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">logged in {session.user.email}</div>
+    </div>
+  )
+ 
+}
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,9 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/app/Layout";
 
 export default function Home() {
-
-  const { data: session } = useSession();
-
-  if(!session){
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={()=> signIn('google')} className="bg-white p-2 rounded-lg hover:bg-red-500 hover:text-white duration-200 hover:scale-110">Login with Google</button>
-        </div>
-      </div>
-    )
-  }
-
   return(
-    <div className="bg-white-900 w-screen h-screen">
-      <div className="">logged in {session.user.email}</div>
-      <button onClick={() => signOut()} className="bg-red-900 text-white p-2 rounded-lg hover:bg-violet-600 duration-200 hover:scale-110">Sign out</button>
-    </div>
+    <Layout>
+      Test
+    </Layout>
   )
- 
 }
